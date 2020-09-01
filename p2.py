3f730691db27bacb9356defa5c24bb5af8f4afb3
Roland Veiderma Holmberg <24371899+rvh97@users.noreply.github.com>
2020-09-01 19:40:13 +0200

    Added skeleton for second practical moment

---
+++ p2.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential, layers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+dataset_dir = 'dataset'
+batch_size = 8
+img_height = 100
+img_width = 100
+
+data_generator = ImageDataGenerator(
+    rescale=1.0/255,
+    validation_split=0.2,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+training_data_generator = data_generator.flow_from_directory(
+    dataset_dir,
+    target_size=(img_height, img_width),
+    subset="training",
+    batch_size=batch_size,
+)
+
+validation_data_generator = data_generator.flow_from_directory(
+    dataset_dir,
+    target_size=(img_height, img_width),
+    subset="validation",
+    batch_size=batch_size,
+)
+
+model = Sequential()
+
+# Write your own code
+# Add your own layers here!
+
+
+
+model.compile(loss='categorical_crossentropy',
+    optimizer='rmsprop', # Try a different optimizer!
+    metrics=['accuracy']
+)
+
+model.summary()
+
+model.fit(
+    training_data_generator,
+    validation_data=validation_data_generator,
+    epochs=3)
